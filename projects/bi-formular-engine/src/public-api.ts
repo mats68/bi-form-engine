777e8d34041cf519ead9a05ec161b45d2bf37021
M. Thaler <mats68@gmail.com>
2020-10-07 13:21:42 +0200

    init

---
+++ projects/bi-formular-engine/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of bi-formular-engine
+ */
+
+export * from './lib/bi-formular-engine.service';
+export * from './lib/bi-formular-engine.component';
+export * from './lib/bi-formular-engine.module';
